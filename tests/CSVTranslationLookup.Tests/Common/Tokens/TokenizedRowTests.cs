9d99d9665139745110106dbe9877f8d697dc1a41
Christopher Whitley <aristurtledev@gmail.com>
2024-02-27 18:45:39 -0500

    Implement tests for TokenizedRow

---
+++ tests/CSVTranslationLookup.Tests/Common/Tokens/TokenizedRowTests.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSVTranslationLookup.Common.Tokens;
+
+namespace CSVTranslationLookup.Tests.Common.Tokens
+{
+    public class TokenizedRowTests
+    {
+        [Fact]
+        public void Equals_True_When_Same_Values()
+        {
+            Token token = new Token(TokenType.Token, "example");
+            TokenizedRow expected = new TokenizedRow(0, new Token[] { token });
+            TokenizedRow actual = new TokenizedRow(0, new Token[] { token });
+            Assert.True(expected.Equals(actual));
+        }
+
+        [Fact]
+        public void Equals_False_When_Different_Values()
+        {
+            Token token = new Token(TokenType.Token, "example");
+            TokenizedRow expected = new TokenizedRow(0, new Token[] { token });
+            TokenizedRow actual = new TokenizedRow(1, new Token[] { token });
+            Assert.False(expected.Equals(actual));
+        }
+
+        [Fact]
+        public void GetHashCode_Same_When_Same_Values()
+        {
+            Token token = new Token(TokenType.Token, "example");
+            int expected = new TokenizedRow(0, new Token[] { token }).GetHashCode();
+            int actual = new TokenizedRow(0, new Token[] { token }).GetHashCode();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetHashCode_Different_When_Different_Values()
+        {
+            Token token = new Token(TokenType.Token, "example");
+            int expected = new TokenizedRow(0, new Token[] { token }).GetHashCode();
+            int actual = new TokenizedRow(1, new Token[] { token }).GetHashCode();
+            Assert.NotEqual(expected, actual);
+        }
+    }
+}
