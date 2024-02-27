983aa5fb660a28bd0b9ff63956f5641c3b6f54dd
Christopher Whitley <aristurtledev@gmail.com>
2024-02-27 17:17:44 -0500

    Added token test for equality

---
+++ tests/CSVTranslationLookup.Tests/Common/Tokens/TokenTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSVTranslationLookup.Common.Tokens;
+
+namespace CSVTranslationLookup.Tests.Common.Tokens
+{
+    public class TokenTests
+    {
+        [Fact]
+        public void Equality()
+        {
+            Token expected = new Token(TokenType.Token, "example");
+            Token actual = new Token(TokenType.Token, "example");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
