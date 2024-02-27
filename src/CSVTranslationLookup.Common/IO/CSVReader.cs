6dc56f78b6d1300e09a8ab1bfb313f45a8f79b37
Christopher Whitley <aristurtledev@gmail.com>
2024-02-27 17:15:59 -0500

    Implement line reading when quoted and contains new lines

--- src/CSVTranslationLookup.Common/IO/CSVReader.cs
+++ src/CSVTranslationLookup.Common/IO/CSVReader.cs
@@ -5,8 +5,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using CSVTranslationLookup.Common.Text;
 using CSVTranslationLookup.Common.Tokens;
 
 namespace CSVTranslationLookup.Common.IO
@@ -15,7 +17,63 @@ namespace CSVTranslationLookup.Common.IO
     {
         public static ParallelQuery<TokenizedRow> FromFile(string filename, char delimiter = ',', char quote = '"')
         {
-            string[] lines = File.ReadAllLines(filename);
+            List<string> lines = new List<string>();
+
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    StringBuilder buffer = StringBuilderCache.Get();
+
+                    bool waitingForQuote = false;
+                    int c;
+
+                    while ((c = reader.Read()) != -1)
+                    {
+                        char character = (char)c;
+
+                        if (!waitingForQuote)
+                        {
+                            if (character == quote)
+                            {
+                                waitingForQuote = true;
+                                continue;
+                            }
+
+                            if (character == '\n')
+                            {
+                                lines.Add(buffer.ToString());
+                                buffer.Clear();
+                            }
+                        }
+                        else
+                        {
+                            if(character == '\n')
+                            {
+                                buffer.AppendLine();
+                                continue;
+                            }
+
+                            if(character == quote)
+                            {
+                                if(reader.Peek() == quote)
+                                {
+                                    buffer.Append(quote);
+                                    _ = reader.Read();  //  Discard the next quote
+                                    continue;
+                                }
+
+                                waitingForQuote = false;
+                                continue;
+                            }
+                        }
+
+                        buffer.Append(character);
+                    }
+                    buffer.Recycle();
+                }
+            }
+
             var query = lines.AsParallel()
                              .AsOrdered()
                              .WithDegreeOfParallelism(Environment.ProcessorCount)
