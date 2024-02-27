543c3e91f31d41723411af090c87b64325fa4d4d
Christopher Whitley <aristurtledev@gmail.com>
2024-02-27 17:17:12 -0500

    Change to `class` type for equality comparison in unit tests

--- src/CSVTranslationLookup.Common/Tokens/Token.cs
+++ src/CSVTranslationLookup.Common/Tokens/Token.cs
@@ -2,13 +2,33 @@
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CSVTranslationLookup.Common.Tokens
 {
-    public record Token(TokenType TokenType, string? Content = null);
+    /// <summary>
+    /// Represents a single token (cel) in a CSV row. 
+    /// </summary>
+    /// <param name="tokenType">The type of token.</param>
+    /// <param name="content">The token contents</param>
+    public class Token(TokenType tokenType, string? content = null) : IEquatable<Token>
+    {
+        /// <summary>
+        /// Gets the type of this token.
+        /// </summary>
+        public TokenType TokenType => tokenType;
+
+        /// <summary>
+        /// Gets the string contents of this token.
+        /// </summary>
+        public string? Content => content;
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj) => obj is Token other && Equals(other);
+
+        /// <inheritdoc />
+        public bool Equals(Token? other) => other is not null && TokenType == other.TokenType && Content == other.Content;
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(TokenType, Content);
+
+    }
 }
--- src/CSVTranslationLookup.Common/Tokens/TokenizedRow.cs
+++ src/CSVTranslationLookup.Common/Tokens/TokenizedRow.cs
@@ -4,5 +4,25 @@
 
 namespace CSVTranslationLookup.Common.Tokens
 {
-    public record TokenizedRow(int Index, Token?[] Tokens);
+    public class TokenizedRow(int index, Token?[] tokens) : IEquatable<TokenizedRow>
+    {
+        /// <summary>
+        /// Gets the index of this row.
+        /// </summary>
+        public int Index => index;
+
+        /// <summary>
+        ///     Gets all tokens in this row.
+        /// </summary>
+        public Token?[] Tokens => tokens;
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj) => obj is TokenizedRow other && Equals(other);
+
+        /// <inheritdoc />
+        public bool Equals(TokenizedRow? other) => other is not null && Index == other.Index && Tokens.SequenceEqual(other.Tokens);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(Index, Tokens);
+    }
 }
