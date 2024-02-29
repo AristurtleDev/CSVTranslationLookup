2be9a2c68f474c6a97e9cba2b1e156bb2bfbb117
Christopher Whitley <aristurtledev@gmail.com>
2024-02-28 22:07:18 -0500

    Added TokenType

---
+++ src/CSVTranslationLookup.Common/Tokens/TokenType.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace CSVTranslationLookup.Common.Tokens
+{
+    /// <summary>
+    /// Specifies the type of token.
+    /// </summary>
+    public enum TokenType
+    {
+        /// <summary>
+        /// A normal token
+        /// </summary>
+        Token,
+
+        /// <summary>
+        /// The final token in a row
+        /// </summary>
+        EndOfRecord
+    }
+}
