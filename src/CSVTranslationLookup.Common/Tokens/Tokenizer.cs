73c75065a29504ad8c9e4a1f7a2968e39e765762
Christopher Whitley <aristurtledev@gmail.com>
2024-02-29 00:10:18 -0500

    Added Tokenizer

---
+++ src/CSVTranslationLookup.Common/Tokens/Tokenizer.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using CSVTranslationLookup.Common.IO;
+
+namespace CSVTranslationLookup.Common.Tokens
+{
+    /// <summary>
+    /// Provides methods for tokenizing a given string based on RFC4180 CSV specifications.
+    /// </summary>
+    public static class Tokenizer
+    {
+        /// <summary>
+        /// Tokenizes the given string based on CSV specification as defined in RFC4180
+        /// </summary>
+        /// <param name="input">The string to tokenize</param>
+        /// <param name="delimiter">The character that represnets a delimiter</param>
+        /// <param name="quote">The character that represents a quote.</param>
+        /// <returns>An array containing the tokens generated</returns>
+        public static Token[] Tokenize(string input, char delimiter = ',', char quote = '"')
+        {
+            using (TokenReader reader = new TokenReader(input, delimiter, quote))
+            {
+                return ReadTokens(reader).ToArray();
+            }
+        }
+
+        private static IList<Token> ReadTokens(TokenReader reader)
+        {
+            IList<Token> tokens = new List<Token>();
+
+            while (true)
+            {
+                Token token = reader.NextToken();
+                tokens.Add(token);
+                if (token.TokenType == TokenType.EndOfRecord)
+                {
+                    break;
+                }
+            }
+
+            return tokens;
+        }
+    }
+}
---
+++ tests/CSVTranslationLookup.Tests/Common/Tokens/TokenizerTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System.Text;
+using CSVTranslationLookup.Common.Tokens;
+
+namespace CSVTranslationLookup.Tests.Common.Tokens
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void Tokenize_Non_Quoted()
+        {
+            Token[] expected = new Token[]
+            {
+                new Token(TokenType.Token, "one"),
+                new Token(TokenType.Token, "two"),
+                new Token(TokenType.EndOfRecord, "three")
+            };
+
+            string line = "one,two,three";
+            Token[] actual = Tokenizer.Tokenize(line);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Tokenize_Quoted()
+        {
+            Token[] expected = new Token[]
+            {
+                new Token(TokenType.Token, "one"),
+                new Token(TokenType.Token, "two"),
+                new Token(TokenType.EndOfRecord, "three")
+            };
+
+            string line = "one,\"two\",three";
+            Token[] actual = Tokenizer.Tokenize(line);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Tokenize_Quoted_NewLines()
+        {
+            Token[] expected = new Token[]
+            {
+                new Token(TokenType.Token, "one"),
+                new Token(TokenType.Token, "two"),
+                new Token(TokenType.EndOfRecord, $"three{Environment.NewLine}four")
+            };
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("one,two,\"three")
+                   .Append("four\"");
+
+            string line = builder.ToString();
+
+            Token[] actual = Tokenizer.Tokenize(line);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
