d70e97e77425c58ad45b33cc2236be5503cef8db
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-12-30 01:02:16 -0500

    Repalce stringreader based tokenization with direct string indexing

--- src/CSVTranslationLookup.Common/Tokens/Tokenizer.cs
+++ src/CSVTranslationLookup.Common/Tokens/Tokenizer.cs
@@ -3,8 +3,8 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
-using System.Linq;
-using CSVTranslationLookup.Common.IO;
+using System.Text;
+using CSVTranslationLookup.Common.Text;
 
 namespace CSVTranslationLookup.Common.Tokens
 {
@@ -13,35 +13,206 @@ namespace CSVTranslationLookup.Common.Tokens
     /// </summary>
     public static class Tokenizer
     {
-        /// <summary>
-        /// Tokenizes the given string based on CSV specification as defined in RFC4180
-        /// </summary>
-        /// <param name="input">The string to tokenize</param>
-        /// <param name="fileName">The file that the input string is from.</param>
-        /// <param name="lineNumber">The line number in the file that the input string is from.</param>
-        /// <param name="delimiter">The character that represnets a delimiter</param>
-        /// <param name="quote">The character that represents a quote.</param>
-        /// <returns>An array containing the tokens generated</returns>
         public static Token[] Tokenize(string input, string fileName, int lineNumber, char delimiter = ',', char quote = '"')
         {
-            using (TokenReader reader = new TokenReader(input, delimiter, quote))
+            if (string.IsNullOrEmpty(input))
             {
-                IList<Token> tokens = new List<Token>();
+                return new Token[] { new Token(TokenType.EndOfRecord) { FileName = fileName, LineNumber = lineNumber } };
+            }
 
-                while (true)
+            // Pre-allocate for common case (most CSV rows are probably 2 to 10 columns)
+            var tokens = new List<Token>(capacity: 8);
+            int position = 0;
+            int length = input.Length;
+
+            while (position < length)
+            {
+                // Skip leading whitespace
+                position = SkipWhitespace(input, position, length, delimiter);
+
+                if (position >= length)
                 {
-                    Token token = reader.NextToken();
-                    token.FileName = fileName;
-                    token.LineNumber = lineNumber;
-                    tokens.Add(token);
-                    if (token.TokenType == TokenType.EndOfRecord)
-                    {
-                        break;
-                    }
+                    // End of input
+                    tokens.Add(new Token(TokenType.EndOfRecord, string.Empty) { FileName = fileName, LineNumber = lineNumber });
+                    break;
                 }
 
-                return tokens.ToArray();
+                char currentChar = input[position];
+
+                // Check for delimiter (empty field)
+                if (currentChar == delimiter)
+                {
+                    tokens.Add(new Token(TokenType.Token, string.Empty) { FileName = fileName, LineNumber = lineNumber });
+
+                    // Skip delimiter
+                    position++;
+                    continue;
+                }
+
+                // Check for quoted field
+                if (currentChar == quote)
+                {
+                    string value = ReadQuotedField(input, ref position, length, quote);
+
+                    // Skip trailing whitespace
+                    position = SkipWhitespace(input, position, length, delimiter);
+
+                    // Determine token type
+                    if (position >= length)
+                    {
+                        tokens.Add(new Token(TokenType.EndOfRecord, value) { FileName = fileName, LineNumber = lineNumber });
+                        break;
+                    }
+                    else if (position < length && input[position] == delimiter)
+                    {
+                        tokens.Add(new Token(TokenType.Token, value) { FileName = fileName, LineNumber = lineNumber });
+
+                        // Skip delimiter
+                        position++;
+                    }
+                    else
+                    {
+                        tokens.Add(new Token(TokenType.Token, value) { FileName = fileName, LineNumber = lineNumber });
+                    }
+                    continue;
+                }
+
+                // Read uquoted field
+                string unquotedvalue = ReadUnquotedField(input, ref position, length, delimiter);
+
+                // Skip trailing whitespace
+                position = SkipWhitespace(input, position, length, delimiter);
+
+                // Determine token type
+                if (position >= length)
+                {
+                    tokens.Add(new Token(TokenType.EndOfRecord, unquotedvalue) { FileName = fileName, LineNumber = lineNumber });
+                    break;
+                }
+                else if (position < length && input[position] == delimiter)
+                {
+                    tokens.Add(new Token(TokenType.Token, unquotedvalue) { FileName = fileName, LineNumber = lineNumber });
+
+                    // Skip delimiter
+                    position++;
+                }
+                else
+                {
+                    tokens.Add(new Token(TokenType.Token, unquotedvalue) { FileName = fileName, LineNumber = lineNumber });
+                }
             }
+
+            // If we ended on a delimiter, add empty EndOfRecord token
+            if (position > 0 && position <= length && input[position - 1] == delimiter)
+            {
+                tokens.Add(new Token(TokenType.EndOfRecord, string.Empty) { FileName = fileName, LineNumber = lineNumber });
+            }
+
+            return tokens.ToArray();
+        }
+
+        /// <summary>
+        /// Skips whitespace characters (space and tab only, not newlines).
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <param name="position">The current position.</param>
+        /// <param name="length">The length of the input string.</param>
+        /// <param name="delimiter">The delimiter character to stop at.</param>
+        /// <returns>The new position after skipping whitespace.</returns>
+        private static int SkipWhitespace(string input, int position, int length, char delimiter)
+        {
+            while (position < length)
+            {
+                char c = input[position];
+                if (c != delimiter && (c == ' ' || c == '\t'))
+                {
+                    position++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return position;
+        }
+
+        /// <summary>
+        /// Reads a quoted field from the input string.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <param name="position">The current position (will be updated)</param>
+        /// <param name="length">The length of the input string.</param>
+        /// <param name="quote">The quote character.</param>
+        /// <returns>The content of the quoted field (without surrounding quotes).</returns>
+        private static string ReadQuotedField(string input, ref int position, int length, char quote)
+        {
+            // Skip opening quote
+            position++;
+
+            StringBuilder buffer = StringBuilderCache.Get();
+            bool insideQutoes = true;
+
+            while (position < length && insideQutoes)
+            {
+                char c = input[position];
+
+                if (c == quote)
+                {
+                    // Check for escaped quote (two consecutive quotes)
+                    if (position + 1 < length && input[position + 1] == quote)
+                    {
+                        // Escaped quote, add single quote to output
+                        buffer.Append(quote);
+
+                        // Skip both quotes
+                        position += 2;
+                    }
+                    else
+                    {
+                        // End of quoted field
+                        // Skip closing quote
+                        position++;
+                        insideQutoes = false;
+                    }
+                }
+                else
+                {
+                    buffer.Append(c);
+                    position++;
+                }
+            }
+
+            return buffer.GetStringAndRecycle();
+        }
+
+        /// <summary>
+        /// Reads an unquoted field form the input string.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <param name="position">The currnet position (will be updated).</param>
+        /// <param name="length">The length of the input string.</param>
+        /// <param name="delimiter">The delimiter character.</param>
+        /// <returns>The content of the unquoted field (trimmed).</returns>
+        private static string ReadUnquotedField(string input, ref int position, int length, char delimiter)
+        {
+            int startPosition = position;
+
+            // Find end of field (delimiter or end of string)
+            while (position < length && input[position] != delimiter)
+            {
+                position++;
+            }
+
+            // Extract substring and trim
+            int fieldLength = position - startPosition;
+            if (fieldLength == 0)
+            {
+                return string.Empty;
+            }
+
+            string value = input.Substring(startPosition, fieldLength);
+            return value.Trim();
         }
     }
 }
--- tests/CSVTranslationLookup.Tests/Common/Tokens/TokenizerTests.cs
+++ tests/CSVTranslationLookup.Tests/Common/Tokens/TokenizerTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using System.Text;
 using CSVTranslationLookup.Common.Tokens;
 
@@ -10,55 +11,309 @@ namespace CSVTranslationLookup.Tests.Common.Tokens
     public class TokenizerTests
     {
         [Fact]
-        public void Tokenize_Non_Quoted()
+        public void Tokenize_Simple_NonQuoted_Fields()
         {
-            Token[] expected = new Token[]
-            {
-                new Token(TokenType.Token, "one"),
-                new Token(TokenType.Token, "two"),
-                new Token(TokenType.EndOfRecord, "three")
-            };
+            string input = "one,two,three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
 
-            string line = "one,two,three";
-            Token[] actual = Tokenizer.Tokenize(line, string.Empty, 0);
-
-            Assert.Equal(expected, actual);
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+            Assert.Equal(TokenType.Token, tokens[0].TokenType);
+            Assert.Equal(TokenType.Token, tokens[1].TokenType);
+            Assert.Equal(TokenType.EndOfRecord, tokens[2].TokenType);
         }
 
         [Fact]
-        public void Tokenize_Quoted()
+        public void Tokenize_Quoted_Fields()
         {
-            Token[] expected = new Token[]
-            {
-                new Token(TokenType.Token, "one"),
-                new Token(TokenType.Token, "two"),
-                new Token(TokenType.EndOfRecord, "three")
-            };
+            string input = "\"one\",\"two\",\"three\"";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
 
-            string line = "one,\"two\",three";
-            Token[] actual = Tokenizer.Tokenize(line, string.Empty, 0);
-
-            Assert.Equal(expected, actual);
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
         }
 
         [Fact]
-        public void Tokenize_Quoted_NewLines()
+        public void Tokenize_Mixed_Quoted_And_NonQuoted()
         {
-            Token[] expected = new Token[]
+            string input = "one,\"two\",three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Empty_Fields()
+        {
+            string input = "one,,three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Empty(tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Trailing_Empty_Field()
+        {
+            string input = "one,two,";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Empty(tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Leading_Whitespace()
+        {
+            string input = "  one,  two,  three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Trailing_Whitespace()
+        {
+            string input = "one  ,two  ,three  ";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Escaped_Quotes()
+        {
+            string input = "\"one \"\"quoted\"\" value\",\"two\",\"three\"";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one \"quoted\" value", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Comma_Inside_Quoted_Field()
+        {
+            string input = "\"one, with comma\",two,three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one, with comma", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Newline_Inside_Quoted_Field()
+        {
+            string input = "\"one\nwith\nnewlines\",two,three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one\nwith\nnewlines", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Empty_String()
+        {
+            string input = "";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Single(tokens);
+            Assert.Equal(TokenType.EndOfRecord, tokens[0].TokenType);
+            Assert.Null(tokens[0].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Null_String()
+        {
+            string input = null;
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Single(tokens);
+            Assert.Equal(TokenType.EndOfRecord, tokens[0].TokenType);
+        }
+
+        [Fact]
+        public void Tokenize_Single_Field()
+        {
+            string input = "single";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Single(tokens);
+            Assert.Equal("single", tokens[0].Content);
+            Assert.Equal(TokenType.EndOfRecord, tokens[0].TokenType);
+        }
+
+        [Fact]
+        public void Tokenize_Sets_FileName_And_LineNumber()
+        {
+            string input = "one,two";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 42);
+
+            Assert.All(tokens, token =>
             {
-                new Token(TokenType.Token, "one"),
-                new Token(TokenType.Token, "two"),
-                new Token(TokenType.EndOfRecord, $"three{Environment.NewLine}four")
-            };
+                Assert.Equal("test.csv", token.FileName);
+                Assert.Equal(42, token.LineNumber);
+            });
+        }
 
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine("one,two,\"three")
-                   .Append("four\"");
+        [Fact]
+        public void Tokenize_EndOfRecord_Token_Is_Last()
+        {
+            string input = "one,two,three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
 
-            string line = builder.ToString();
+            Assert.Equal(TokenType.EndOfRecord, tokens[^1].TokenType);
+            Assert.All(tokens.Take(tokens.Length - 1), token =>
+            {
+                Assert.Equal(TokenType.Token, token.TokenType);
+            });
+        }
 
-            Token[] actual = Tokenizer.Tokenize(line, string.Empty, 0);
-            Assert.Equal(expected, actual);
+        [Fact]
+        public void Tokenize_Custom_Delimiter()
+        {
+            string input = "one|two|three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1, delimiter: '|');
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Custom_Quote()
+        {
+            string input = "'one','two','three'";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1, quote: '\'');
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Complex_Escaped_Quotes()
+        {
+            string input = "\"He said \"\"Hello\"\" to me\",\"She said \"\"Hi\"\" back\"";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(2, tokens.Length);
+            Assert.Equal("He said \"Hello\" to me", tokens[0].Content);
+            Assert.Equal("She said \"Hi\" back", tokens[1].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Only_Delimiter()
+        {
+            string input = ",";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(2, tokens.Length);
+            Assert.Empty(tokens[0].Content);
+            Assert.Empty(tokens[1].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Multiple_Delimiters()
+        {
+            string input = ",,,";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+
+            Assert.Equal(4, tokens.Length);
+            Assert.All(tokens, token => Assert.Empty(token.Content));
+        }
+
+        [Fact]
+        public void Tokenize_Tab_Delimiter()
+        {
+            string input = "one\ttwo\tthree";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1, delimiter: '\t');
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Tokenize_Semicolon_Delimiter()
+        {
+            string input = "one;two;three";
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1, delimiter: ';');
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("one", tokens[0].Content);
+            Assert.Equal("two", tokens[1].Content);
+            Assert.Equal("three", tokens[2].Content);
+        }
+
+        [Fact]
+        public void Performance_Test_Large_Row()
+        {
+            // Create a row with 100 fields
+            var fields = Enumerable.Range(1, 100).Select(i => $"field{i}");
+            string input = string.Join(",", fields);
+
+            var sw = Stopwatch.StartNew();
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+            sw.Stop();
+
+            Assert.Equal(100, tokens.Length);
+            Assert.True(sw.ElapsedMilliseconds < 10, $"Tokenizing 100 fields took {sw.ElapsedMilliseconds}ms (should be <10ms)");
+        }
+
+        [Fact]
+        public void Performance_Test_Many_Quoted_Fields()
+        {
+            // Create a row with 50 quoted fields
+            var fields = Enumerable.Range(1, 50).Select(i => $"\"field {i} with spaces\"");
+            string input = string.Join(",", fields);
+
+            var sw = Stopwatch.StartNew();
+            Token[] tokens = Tokenizer.Tokenize(input, "test.csv", 1);
+            sw.Stop();
+
+            Assert.Equal(50, tokens.Length);
+            Assert.True(sw.ElapsedMilliseconds < 10, $"Tokenizing 50 quoted fields took {sw.ElapsedMilliseconds}ms (should be <10ms)");
+        }
+
+        [Fact]
+        public void Tokenize_Real_World_CSV_Row()
+        {
+            // Real-world example from game localization
+            string input = "ABILITY_ATTACK,Attack,\"Deal 5 damage to target enemy. This damage ignores armor.\"";
+            Token[] tokens = Tokenizer.Tokenize(input, "abilities.csv", 10);
+
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal("ABILITY_ATTACK", tokens[0].Content);
+            Assert.Equal("Attack", tokens[1].Content);
+            Assert.Equal("Deal 5 damage to target enemy. This damage ignores armor.", tokens[2].Content);
+            Assert.Equal("abilities.csv", tokens[0].FileName);
+            Assert.Equal(10, tokens[0].LineNumber);
         }
     }
 }
